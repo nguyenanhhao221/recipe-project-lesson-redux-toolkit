bb18dd1848b43a4ffbc1033e8530feffd46705c5
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-04-13 18:44:44 +0700

    changes img link

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 const allRecipesData = [
-    { id: 0, name: 'Biscuits', img: './img/biscuits.jpg'},
-    { id: 1, name: 'Bulgogi', img: 'img/bulgogi.jpg'},
-    { id: 2, name: 'Calamari', img: 'img/calamari.jpg'},
-    { id: 3, name: 'Ceviche', img: 'img/ceviche.jpg'},
-    { id: 4, name: 'Cheeseburger', img: 'img/cheeseburger.jpg'},
-    { id: 5, name: 'Churrasco', img: 'img/churrasco.jpg'},
-    { id: 6, name: 'Dumplings', img: 'img/dumplings.jpg'},
-    { id: 7, name: 'Fish & Chips', img: 'img/fishnchips.jpg'},
-    { id: 8, name: 'Hummus', img: 'img/hummus.jpg'},
-    { id: 9, name: 'Masala Dosa', img: 'img/masaladosa.jpg'},
-    { id: 10, name: 'Pad Thai', img: 'img/padthai.jpg'},
+    { id: 0, name: 'Biscuits', img: 'https://i.imgur.com/IdkaaT7.jpeg'},
+    { id: 1, name: 'Bulgogi', img: 'https://i.imgur.com/CEmchNd.jpg'},
+    { id: 2, name: 'Calamari', img: 'https://i.imgur.com/UaeVd8l.jpg'},
+    { id: 3, name: 'Ceviche', img: 'https://i.imgur.com/GVQaCnG.jpg'},
+    { id: 4, name: 'Cheeseburger', img: 'https://i.imgur.com/kLUBMc2.jpg'},
+    { id: 5, name: 'Churrasco', img: 'https://i.imgur.com/vvOBobN.jpg'},
+    { id: 6, name: 'Dumplings', img: 'https://i.imgur.com/PChYqR1.jpg'},
+    { id: 7, name: 'Fish & Chips', img: 'https://i.imgur.com/i5mZX1X.jpg'},
+    { id: 8, name: 'Hummus', img: 'https://i.imgur.com/Esci1Ft.jpg'},
+    { id: 9, name: 'Masala Dosa', img: 'https://i.imgur.com/TILPRgc.jpg'},
+    { id: 10, name: 'Pad Thai', img: 'https://i.imgur.com/W45uWC2.jpg'},
   ];
   
   export default allRecipesData;
